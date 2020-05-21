e2283441bcfd0f2054aec80fa1991b875453ee37
antonkondrat <antonboec@mail.ru>
2020-05-21 22:43:20 +0300

    added featured

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
-$(function(){
+$(function () {
 
+   $('.featured__slider-inner').slick({
+      // infinite: true,
+      // speed: 1000,
+      // fade: true,
+      // cssEase: 'linear'
+      nextArrow: '<i class="fas fa-chevron-right"></i>',
+      prevArrow: '<i class="fas fa-chevron-left"></i>'
+   });
 
+   $(".rateyo").rateYo({
+      rating: 4.5,
+      starWidth: "15px",
+      readOnly: true,
+      spacing: "4px"
+   });
 
 });
